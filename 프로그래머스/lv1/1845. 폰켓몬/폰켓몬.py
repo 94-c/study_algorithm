36f852c8b9872f8e5346388f513313018ee0a274
94-c <hyeongwoo26@gmail.com>
2023-09-22 14:02:26 +0900

    [level 1] Title: 폰켓몬, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,9 @@
 def solution(nums):
+    # nums 리스트의 고유한 원소 개수 구한다.
     answer = len(set(nums))
+    
+    # answer 보다 nums의 절반보다 크면,
+    # nums의 길이의 절반을 반환합니다.
     if answer > len(nums) / 2:
         return len(nums) /2
     return answer
