815a4e0f0016a9f7d6352bae5f35b732b9961df6
94-c <hyeongwoo26@gmail.com>
2023-10-20 13:16:55 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 133.92 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,17 +1,3 @@
-#function solution(arr)
-#{
-#    var answer = [];
-#
-#    for(var i=0; i<arr.length; i++) {
-#        //중복확인
-#        if(arr[i] !== arr[i+1]) {
-#           //중복 되지 않은 요소 추가
-#            answer.push(arr[i]);
-#        }
-#    }
-#    return answer;
-#}
-
 def solution(arr):
     # 결과를 저장할 빈 리스트를 생성
     answer = []
